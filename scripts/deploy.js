6fdaaa4efb7f047ed5f0a33de115af9cf8d66afa
Hugo Nogueira <hugomn@gmail.com>
2018-08-09 15:27:14 +0200

    Updates

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,5 +1,6 @@
 var ghpages = require('gh-pages');
 
+// TODO: improve output https://github.com/tschaub/gh-pages
 ghpages.publish('_site', {
     branch: 'master',
     repo: 'git@github.com:hugomn/hugomn.github.io.git'
